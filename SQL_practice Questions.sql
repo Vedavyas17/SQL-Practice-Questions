008abf277c499f0caf630769a47553bba05903d4
Vedavyas Chakkirala <59414826+Vedavyas17@users.noreply.github.com>
2024-05-12 15:03:27 -0400

    Update SQL_practice Questions.sql

--- SQL_practice Questions.sql
+++ SQL_practice Questions.sql
@@ -77,4 +77,34 @@ case when s1 is null then s2
 else s1
 end as salary
 from cte
+-----------------problem 3--------------------------
+CREATE TABLE city_distance
+(
+distance INT,
+source VARCHAR(512),
+destination VARCHAR(512)
+);
+
+delete from city_distance;
+INSERT INTO city_distance(distance, source, destination) VALUES ('100', 'New Delhi', 'Panipat');
+INSERT INTO city_distance(distance, source, destination) VALUES ('200', 'Ambala', 'New Delhi');
+INSERT INTO city_distance(distance, source, destination) VALUES ('150', 'Bangalore', 'Mysore');
+INSERT INTO city_distance(distance, source, destination) VALUES ('150', 'Mysore', 'Bangalore');
+INSERT INTO city_distance(distance, source, destination) VALUES ('250', 'Mumbai', 'Pune');
+INSERT INTO city_distance(distance, source, destination) VALUES ('250', 'Pune', 'Mumbai');
+INSERT INTO city_distance(distance, source, destination) VALUES ('2500', 'Chennai', 'Bhopal');
+INSERT INTO city_distance(distance, source, destination) VALUES ('2500', 'Bhopal', 'Chennai');
+INSERT INTO city_distance(distance, source, destination) VALUES ('60', 'Tirupati', 'Tirumala');
+INSERT INTO city_distance(distance, source, destination) VALUES ('80', 'Tirumala', 'Tirupati');
+INSERT INTO city_distance(distance, source, destination) VALUES ('2500', 'x', 'y');
+
+with cte as (select distance,lead(distance) over(partition by distance) as d,
+			 source,lead(source) over(partition by distance) as s1,
+			 destination,lead(destination) over(partition by distance) as d1
+from city_distance)
+
+select * from city_distance
+except
+select distance,source,destination
+from cte where s1 = destination and d1 = source and d = distance
 
