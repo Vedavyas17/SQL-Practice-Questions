10e6fbb63ebcbe0e7322597727150dc18376d84b
Vedavyas Chakkirala <59414826+Vedavyas17@users.noreply.github.com>
2024-06-07 10:51:04 -0400

    Update SQL_practice Questions.sql

--- SQL_practice Questions.sql
+++ SQL_practice Questions.sql
@@ -169,4 +169,19 @@ from cte2 group by group_key,service_name )
 select service_name,service_status,start_updated_time,end_updated_time from cte3 
 where extract(minute from (end_updated_time - start_updated_time))>=3;
 
+------------------------------------------------------------------
+-- Ankit Bansal SQL Question from Accenture
+CREATE TABLE employees  (employee_id int,employee_name varchar(15), email_id varchar(15) );
+delete from employees;
+INSERT INTO employees (employee_id,employee_name, email_id) VALUES ('101','Liam Alton', 'li.al@abc.com');
+INSERT INTO employees (employee_id,employee_name, email_id) VALUES ('102','Josh Day', 'jo.da@abc.com');
+INSERT INTO employees (employee_id,employee_name, email_id) VALUES ('103','Sean Mann', 'se.ma@abc.com'); 
+INSERT INTO employees (employee_id,employee_name, email_id) VALUES ('104','Evan Blake', 'ev.bl@abc.com');
+INSERT INTO employees (employee_id,employee_name, email_id) VALUES ('105','Toby Scott', 'jo.da@abc.com');
+INSERT INTO employees (employee_id,employee_name, email_id) VALUES ('106','Anjali Chouhan', 'JO.DA@ABC.COM');
+INSERT INTO employees (employee_id,employee_name, email_id) VALUES ('107','Ankit Bansal', 'AN.BA@ABC.COM');
 
+with cte as(select *,row_number() over(partition by lower(email_id)) as ref from employees),
+cte1 as (select *,case when ref>1 and email_id<>lower(email_id) then 0 else 1 end as x
+from cte) 
+select employee_id,employee_name, email_id from cte1 where x=1
