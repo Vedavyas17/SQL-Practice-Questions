cae97529a2f7e5fedb4e9d4a32fdae103db5c3a9
Vedavyas Chakkirala <59414826+Vedavyas17@users.noreply.github.com>
2024-07-04 11:29:47 -0400

    Update SQL_practice Questions.sql

--- SQL_practice Questions.sql
+++ SQL_practice Questions.sql
@@ -430,3 +430,34 @@ max(seat_id) over(partition by cnt)as end,
 count(cnt) over(partition by cnt) as seats_count from cte1),
 cte3 as(select *,dense_rank() over(order by seats_count desc) as rnk from cte2)
 select * from cte3 where rnk=1
+---------------------TechTFQ 13--------------------------------------------
+-- Find out the no of employees managed by each manager.
+
+drop table if exists employee_managers;
+create table employee_managers
+(
+	id			int,
+	name		varchar(20),
+	manager 	int
+);
+insert into employee_managers values (1, 'Sundar', null);
+insert into employee_managers values (2, 'Kent', 1);
+insert into employee_managers values (3, 'Ruth', 1);
+insert into employee_managers values (4, 'Alison', 1);
+insert into employee_managers values (5, 'Clay', 2);
+insert into employee_managers values (6, 'Ana', 2);
+insert into employee_managers values (7, 'Philipp', 3);
+insert into employee_managers values (8, 'Prabhakar', 4);
+insert into employee_managers values (9, 'Hiroshi', 4);
+insert into employee_managers values (10, 'Jeff', 4);
+insert into employee_managers values (11, 'Thomas', 1);
+insert into employee_managers values (12, 'John', 15);
+insert into employee_managers values (13, 'Susan', 15);
+insert into employee_managers values (14, 'Lorraine', 15);
+insert into employee_managers values (15, 'Larry', 1);
+
+select * from employee_managers;
+
+select e2.name,count(e2.name) from employee_managers e1 
+join employee_managers e2 on e1.manager=e2.id
+group by e2.name order by 2 desc
